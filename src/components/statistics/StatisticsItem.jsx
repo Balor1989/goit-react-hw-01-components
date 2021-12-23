c941156b0a140c58d30487030e17e385584b1565
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-23 15:33:09 +0200

    Create StatisticsItem.jsx

---
+++ src/components/statistics/StatisticsItem.jsx
@@ -0,0 +1,19 @@
+import propTypes from "prop-types"
+
+function StatisticsItem(props) {
+   const { id, label, percentage} = props
+    return (
+            <li className="item" key={id}>
+                <span className="label">{label}</span>
+                <span className="percentage">{percentage}</span>
+            </li>
+    )
+
+}
+
+export default StatisticsItem
+
+StatisticsItem.propTypes = {
+    label: propTypes.string.isRequired,
+    percentage: propTypes.number.isRequired
+}
